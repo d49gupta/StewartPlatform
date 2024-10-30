147bdb44d62a5f948571a110a6028ac26231e89d
Dharma Gupta <dharma.anita@gmail.com>
2024-10-29 22:37:32 -0400

    fixed small thing

--- Software/RaspberryPi/ballTracking.py
+++ Software/RaspberryPi/ballTracking.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 from time import sleep
-from StewartPlatform.Software.RaspberryPi.inverseKinematics import PID
+from inverseKinematics import PID
     
 if __name__ == '__main__':
     cap = cv.VideoCapture(0)
