6c98f6636c48587b760abc2a8809b6ff6c2f5d95
Dharma Gupta <dharma.anita@gmail.com>
2024-11-02 16:32:22 -0400

    added PID calculations for ball tracking

--- Software/RaspberryPi/PID_Calculations.py
+++ Software/RaspberryPi/PID_Calculations.py
@@ -1,24 +1,7 @@
 import numpy as np
-import math
+import config
 import matplotlib.pyplot as plt
 import time
-
-x_desired = 0
-y_desired = 0
-Kp = 1.0
-Kd = 0.025
-Ki = 0.5
-
-integral_x = 0.0
-integral_y = 0.0
-errorPrev_x = 0
-errorPrev_y = 0
-previousT = 0
-
-integral_max = 100
-integral_min = -100
-max_limit = 25
-min_limit = -25
 
 def getTime():
     currentTime = time.time()
@@ -27,20 +10,22 @@ def getTime():
 
     return deltaT
 
-def PID(current_x, current_y):
+def PID(current_x, current_y, height, width):
+    x_desired = width / 2
+    y_desired = height / 2
     error_x = current_x - x_desired
     error_y = current_y - y_desired
     deltaT = getTime()
 
-    integral_x = max(min((integral_x + error_x*deltaT), integral_max), integral_min)
-    integral_y =  max(min((integral_y + error_y*deltaT), integral_max), integral_min)
+    integral_x = max(min((integral_x + error_x*deltaT), config.integral_max), config.integral_min)
+    integral_y =  max(min((integral_y + error_y*deltaT), config.integral_max), config.integral_min)
 
     derivative_x = (error_x - errorPrev_x)/deltaT
     derivative_y = (error_y - errorPrev_y)/deltaT
     errorPrev_x = error_x
     errorPrev_y = error_y
 
-    pitch = max(min((Kp*error_x + Kd*derivative_x + Ki*integral_x), max_limit), min_limit)
-    roll =  max(min((Kp*error_y + Kd*derivative_y + Ki*integral_y), max_limit), min_limit)
+    pitch = max(min((config.Kp*error_x + config.Kd*derivative_x + config.Ki*integral_x), config.max_rotation_limit), config.min_rotation_limit)
+    roll =  max(min((config.Kp*error_y + config.Kd*derivative_y + config.Ki*integral_y), config.max_rotation_limit), config.min_rotation_limit)
 
     return pitch, roll
---
+++ Software/RaspberryPi/config.py
@@ -0,0 +1,37 @@
+from smbus2 import SMBus
+import numpy as np
+
+Kp = 1.0
+Kd = 0.025
+Ki = 0.5
+
+integral_x = 0.0
+integral_y = 0.0
+errorPrev_x = 0
+errorPrev_y = 0
+previousT = 0
+
+integral_max = 100
+integral_min = -100
+max_rotation_limit = 25
+min_rotation_limit = -25
+
+addr = 0x8 
+bus = SMBus(1)
+
+motors = 3
+radius = 5
+
+# Servo Motors
+leg_length = 5
+beta = 20
+flapVector = [1.8, 0, 1.575]
+
+# Stepper Motors
+legLength1 = 20
+legLength2 = 20
+phi_zero = 25
+
+section_angle = 360 / motors
+base_motors = np.empty((motors, 3))
+platform_motors = np.empty((motors, 3))
