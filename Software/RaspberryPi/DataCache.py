6ced5f02746941ab7c6208cba9ee497bc2e9e92d
Dharma Gupta <dharma.anita@gmail.com>
2024-11-12 21:42:07 -0500

    added DataCache file

---
+++ Software/RaspberryPi/DataCache.py
@@ -0,0 +1,71 @@
+from loggingModule import logger as lg
+import threading
+
+class CircularBuffer:
+    def __init__(self, size):
+        self.buffer = [None] * size
+        self.size = size
+        self.head = 0  # Write pointer (newest element in data queue)
+        self.tail = 0  # Read pointer (oldest element in data queue)
+        self.count = 0  # Track number of elements in the buffer
+
+    def is_full(self):
+        return self.count == self.size
+
+    def is_empty(self):
+        return self.count == 0
+
+    def enqueue(self, data):
+        self.buffer[self.head] = data
+        self.head = (self.head + 1) % self.size
+        if self.is_full():
+            self.tail = (self.tail + 1) % self.size
+        else:
+            self.count += 1
+
+    def dequeue(self):
+        if not self.is_empty():
+            data = self.buffer[self.tail]
+            self.buffer[self.tail] = None
+            self.tail = (self.tail + 1) % self.size
+            self.count -= 1
+            return data
+        else:
+            lg.fatal("Buffer is empty. Cannot dequeue.")
+            return None
+
+    def oldestValue(self):
+        if not self.is_empty():
+            return self.buffer[self.tail]
+        else:
+            lg.warning("Buffer is empty. Nothing to peek at.")
+            return None
+    
+    def newestValue(self):
+        if not self.is_empty():
+            newest_index = (self.head - 1) % self.size
+            return self.buffer[newest_index]
+        else:
+            lg.warning("Buffer is empty. Nothing to peek at.")
+            return None
+
+    def __str__(self):
+        return f"CircularBuffer({self.buffer})" # used to print object CircularBuffer
+
+if __name__ == '__main__':
+    buffer_size = 5
+    cb = CircularBuffer(buffer_size)
+
+    cb.enqueue(10)
+    cb.enqueue(20)
+    cb.enqueue(30)
+    print(cb)
+
+    print("Dequeued:", cb.dequeue())
+    print(cb)
+
+    print("Peek:", cb.oldestValue())
+    cb.enqueue(49)
+
+    print("Snoop:", cb.newestValue())
+    print(cb)
--- Software/RaspberryPi/MPU6050.py
+++ Software/RaspberryPi/MPU6050.py
@@ -3,120 +3,137 @@ from PID_Calculations import getTime
 import math
 import config
 from loggingModule import logger as lg
+from DataCache import CircularBuffer
+import threading
+class MPU6050:
+    PWR_MGMT_1   = 0x6B
+    SMPLRT_DIV   = 0x19
+    CONFIG       = 0x1A
+    GYRO_CONFIG  = 0x1B
+    INT_ENABLE   = 0x38
+    ACCEL_XOUT_H = 0x3B
+    ACCEL_YOUT_H = 0x3D
+    ACCEL_ZOUT_H = 0x3F
+    GYRO_XOUT_H  = 0x43
+    GYRO_YOUT_H  = 0x45
+    GYRO_ZOUT_H  = 0x47
 
-PWR_MGMT_1   = 0x6B
-SMPLRT_DIV   = 0x19
-CONFIG       = 0x1A
-GYRO_CONFIG  = 0x1B
-INT_ENABLE   = 0x38
-ACCEL_XOUT_H = 0x3B
-ACCEL_YOUT_H = 0x3D
-ACCEL_ZOUT_H = 0x3F
-GYRO_XOUT_H  = 0x43
-GYRO_YOUT_H  = 0x45
-GYRO_ZOUT_H  = 0x47
+    gyroAngleX, gyroAngleY, gyroAngleZ, yaw = 0, 0, 0, 0
+    AccErrorX, AccErrorY, GyroErrorX, GyroErrorY, GyroErrorZ = 0, 0, 0, 0, 0
+    bufferSize = 10
 
-gyroAngleX, gyroAngleY, gyroAngleZ, yaw = 0, 0, 0, 0
-AccErrorX, AccErrorY, GyroErrorX, GyroErrorY, GyroErrorZ = 0, 0, 0, 0, 0
+    def __init__(self):
+        self.bus = SMBus(1)
 
-def MPU_Init():
-	#write to sample rate register
-	bus.write_byte_data(config.imu_addr, SMPLRT_DIV, 7)
-	
-	#Write to power management register
-	bus.write_byte_data(config.imu_addr, PWR_MGMT_1, 1)
-	
-	#Write to Configuration register
-	bus.write_byte_data(config.imu_addr, CONFIG, 0)
-	
-	#Write to Gyro configuration register
-	bus.write_byte_data(config.imu_addr, GYRO_CONFIG, 24)
-	
-	#Write to interrupt enable register
-	bus.write_byte_data(config.imu_addr, INT_ENABLE, 1)
+        self.bus.write_byte_data(config.imu_addr, self.SMPLRT_DIV, 7) # write to sample rate register
+        self.bus.write_byte_data(config.imu_addr, self.PWR_MGMT_1, 1) # write to power management register
+        self.bus.write_byte_data(config.imu_addr, self.CONFIG, 0) # write to configuration register
+        self.bus.write_byte_data(config.imu_addr, self.GYRO_CONFIG, 24) # write to gyro configuration register
+        self.bus.write_byte_data(config.imu_addr, self.INT_ENABLE, 1) # write to interrupt enable register
 
-def read_raw_data(addr, gyro = True):
-	#Accelero and Gyro value are 16-bit
-    high = bus.read_byte_data(config.imu_addr, addr)
-    low = bus.read_byte_data(config.imu_addr, addr+1)
+        self.dataCache = CircularBuffer(self.bufferSize)
+        self.mutex = threading.lock()
+        self.stop_flag = threading.Event() # flag to stop threads if needed
 
-    #concatenate higher and lower value
-    value = ((high << 8) | low)
-    
-    #to get signed value from mpu6050
-    if(value > 32768):
-            value = value - 65536
-            lg.debug("Signed Value Adjusted to %f", value)
-    
-    if gyro:
-         return value / 131.0
-    else:
-         return value / 16384.0
+    def read_raw_data(self, addr, gyro = True):
+        #Accelero and Gyro value are 16-bit
+        high = self.bus.read_byte_data(config.imu_addr, addr)
+        low = self.bus.read_byte_data(config.imu_addr, addr+1)
 
-def calculateOrientation(acc_x, acc_y, gyro_x, gyro_y, acc_z, gyro_z):
-    global gyroAngleX, gyroAngleY, gyroAngleZ, yaw
-    accAngleX = (math.atan(acc_y / math.sqrt(pow(acc_x, 2) + pow(acc_z, 2))) * 180 / math.pi) - AccErrorX # choose to keep AccErrorX/AccErrorY only for increased accuracy, if too slow get rid of calculation
-    accAngleY = (math.atan(acc_x / math.sqrt(pow(acc_y, 2) + pow(acc_z, 2))) * 180 / math.pi) + AccErrorY
-    gyro_x = gyro_x + GyroErrorX
-    gyro_y = gyro_y - GyroErrorY
-    gyro_z = gyro_z + GyroErrorZ
-
-    elapsedTime = getTime()
-    gyroAngleX = gyroAngleX + gyro_x * elapsedTime 
-    gyroAngleY = gyroAngleY + gyro_y * elapsedTime
-    yaw =  yaw + gyro_z * elapsedTime
-    roll = 0.96 * gyroAngleX + 0.04 * accAngleX
-    pitch = 0.96 * gyroAngleY + 0.04 * accAngleY
-
-    lg.info("Pitch=%.2f%s\tRoll=%.2f%s\tYaw=%.2f%s" % (pitch, u'\u00b0', roll, u'\u00b0', yaw, u'\u00b0'))
-    return pitch, roll, yaw
-
-def calibration():
-    global AccErrorX, AccErrorY, GyroErrorX, GyroErrorY, GyroErrorZ
-    counter = 0
-    while (counter < 200):
-        acc_x = read_raw_data(ACCEL_XOUT_H, False)
-        acc_y = read_raw_data(ACCEL_YOUT_H, False)
-        acc_z = read_raw_data(ACCEL_ZOUT_H, False)
-        gyro_x = read_raw_data(GYRO_XOUT_H, True)
-        gyro_y = read_raw_data(GYRO_YOUT_H, True)
-        gyro_z = read_raw_data(GYRO_ZOUT_H, True)
-
-        AccErrorX = AccErrorX + ((math.atan((acc_y) / math.sqrt(pow((acc_x), 2) + pow((acc_z), 2))) * 180 / math.pi))
-        AccErrorY = AccErrorY + ((math.atan(-1 * (acc_x) / math.sqrt(pow((acc_y), 2) + pow((acc_z), 2))) * 180 / math.pi))
-        GyroErrorX = GyroErrorX + (gyro_x)
-        GyroErrorY = GyroErrorY + (gyro_y)
-        GyroErrorZ = GyroErrorZ + (gyro_z)
-        counter += 1
-
-    AccErrorX = AccErrorX / 200
-    AccErrorY = AccErrorY / 200
-    GyroErrorX - GyroErrorX / 200
-    GyroErrorY - GyroErrorY / 200
-    GyroErrorZ - GyroErrorZ / 200
-
-def simpleCalculation(acc_x, acc_y, acc_z):
-    roll = math.atan2(acc_y, acc_z)*180/math.pi
-    pitch = ((math.atan(acc_x / math.sqrt(pow((acc_y), 2) + pow((acc_z), 2))) * 180 / math.pi))
-
-    lg.info("Pitch=%.2f%s\tRoll=%.2f%s" % (pitch, u'\u00b0', roll, u'\u00b0'))
-    return roll, pitch
-      
-if __name__ == '__main__':
-    bus = SMBus(1)
-
-    MPU_Init()
-    lg.debug("Calibrating IMU")
-    calibration()
-    lg.debug("Reading Data of Gyroscope and Accelerometer")
-
-    while True:
-        acc_x = read_raw_data(ACCEL_XOUT_H, False)
-        acc_y = read_raw_data(ACCEL_YOUT_H, False)
-        acc_z = read_raw_data(ACCEL_ZOUT_H, False)
-        gyro_x = read_raw_data(GYRO_XOUT_H, True)
-        gyro_y = read_raw_data(GYRO_YOUT_H, True)
-        gyro_z = read_raw_data(GYRO_ZOUT_H, True)
+        #concatenate higher and lower value
+        value = ((high << 8) | low)
         
-        lg.debug("Gx=%.2f%s/s\tGy=%.2f%s/s\tGz=%.2f%s/s\tAx=%.2f g\tAy=%.2f g\tAz=%.2f g" % (gyro_x, u'\u00b0', gyro_y, u'\u00b0', gyro_z, u'\u00b0', acc_x, acc_y, acc_z))
-        roll1, pitch1 = simpleCalculation(acc_x, acc_y, acc_z)
+        #to get signed value from mpu6050
+        if(value > 32768):
+                value = value - 65536
+                lg.debug("Signed Value Adjusted to %f", value)
+        
+        if gyro:
+            return value / 131.0
+        else:
+            return value / 16384.0
+
+    def calculateOrientation(self, acc_x, acc_y, gyro_x, gyro_y, acc_z, gyro_z):
+        global gyroAngleX, gyroAngleY, gyroAngleZ, yaw
+        accAngleX = (math.atan(acc_y / math.sqrt(pow(acc_x, 2) + pow(acc_z, 2))) * 180 / math.pi) - self.AccErrorX # choose to keep AccErrorX/AccErrorY only for increased accuracy, if too slow get rid of calculation
+        accAngleY = (math.atan(acc_x / math.sqrt(pow(acc_y, 2) + pow(acc_z, 2))) * 180 / math.pi) + self.AccErrorY
+        gyro_x = gyro_x + self.GyroErrorX
+        gyro_y = gyro_y - self.GyroErrorY
+        gyro_z = gyro_z + self.GyroErrorZ
+
+        elapsedTime = getTime()
+        gyroAngleX = gyroAngleX + gyro_x * elapsedTime 
+        gyroAngleY = gyroAngleY + gyro_y * elapsedTime
+        yaw =  yaw + gyro_z * elapsedTime
+        roll = 0.96 * gyroAngleX + 0.04 * accAngleX
+        pitch = 0.96 * gyroAngleY + 0.04 * accAngleY
+
+        lg.info("Pitch=%.2f%s\tRoll=%.2f%s\tYaw=%.2f%s" % (pitch, u'\u00b0', roll, u'\u00b0', yaw, u'\u00b0'))
+        return pitch, roll, yaw
+
+    def calibration(self):
+        counter = 0
+        while (counter < 200):
+            acc_x = self.read_raw_data(self.ACCEL_XOUT_H, False)
+            acc_y = self.read_raw_data(self.ACCEL_YOUT_H, False)
+            acc_z = self.read_raw_data(self.ACCEL_ZOUT_H, False)
+            gyro_x = self.read_raw_data(self.GYRO_XOUT_H, True)
+            gyro_y = self.read_raw_data(self.GYRO_YOUT_H, True)
+            gyro_z = self.read_raw_data(self.GYRO_ZOUT_H, True)
+
+            lg.debug("Gx=%.2f%s/s\tGy=%.2f%s/s\tGz=%.2f%s/s\tAx=%.2f g\tAy=%.2f g\tAz=%.2f g" % (gyro_x, u'\u00b0', gyro_y, u'\u00b0', gyro_z, u'\u00b0', acc_x, acc_y, acc_z))
+
+            self.AccErrorX = self.AccErrorX + ((math.atan((acc_y) / math.sqrt(pow((acc_x), 2) + pow((acc_z), 2))) * 180 / math.pi))
+            self.AccErrorY = self.AccErrorY + ((math.atan(-1 * (acc_x) / math.sqrt(pow((acc_y), 2) + pow((acc_z), 2))) * 180 / math.pi))
+            self.GyroErrorX = self.GyroErrorX + (gyro_x)
+            self.GyroErrorY = self.GyroErrorY + (gyro_y)
+            self.GyroErrorZ = self.GyroErrorZ + (gyro_z)
+            counter += 1
+
+        self.AccErrorX = self.AccErrorX / 200
+        self.AccErrorY = self.AccErrorY / 200
+        self.GyroErrorX = self.GyroErrorX / 200
+        self.GyroErrorY = self.GyroErrorY / 200
+        self.GyroErrorZ = self.GyroErrorZ / 200
+
+    def calculatePitchRoll(self):
+        acc_x = self.read_raw_data(self.ACCEL_XOUT_H, False)
+        acc_y = self.read_raw_data(self.ACCEL_YOUT_H, False)
+        acc_z = self.read_raw_data(self.ACCEL_ZOUT_H, False)
+        roll = math.atan2(acc_y, acc_z)*180/math.pi
+        pitch = ((math.atan(acc_x / math.sqrt(pow((acc_y), 2) + pow((acc_z), 2))) * 180 / math.pi))
+
+        lg.info("Pitch=%.2f%s\tRoll=%.2f%s" % (pitch, u'\u00b0', roll, u'\u00b0'))
+        return (pitch, roll)
+    
+    def cacheSensorReadings(self):
+        while not self.stop_flag.is_set(): # keep running until stop_flag is set
+            with self.mutex:
+                sensorData = self.calculateOrientation()
+                self.dataCache.enqueue(sensorData)
+
+    def readCacheReadings(self):
+        while not self.stop_flag.is_set(): # keep running until stop_flag is set
+            with self.mutex:
+                val = self.dataCache.newestValue()
+                # write to arduino
+                lg.info("Pitch=%.2f%s\tRoll=%.2f%s" % (val[0], u'\u00b0', val[1], u'\u00b0'))
+                return val
+    
+    def stop(self):
+        self.stop_flag.set()  # Signal both threads to stop
+
+    def startStreamingIMU(self):
+        producerThread = threading.Thread(target=self.cacheSensorReadings)
+        consumerThread = threading.Thread(target=self.readCacheReadings)
+
+        producerThread.start()
+        consumerThread.start()
+
+        producerThread.join()
+        consumerThread.join()
+
+        
+if __name__ == '__main__':
+    imu = MPU6050()
+    
